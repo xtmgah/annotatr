30fd8df96d55f7902a5f9de58f0a32b4d25226e1
hhabra <hhabra1@gmail.com>
2015-11-28 15:49:47 -0500

    Read function, update #2

--- R/Read.R
+++ R/Read.R
@@ -1,46 +1,60 @@
-library(readr)
-library(GenomicRanges)
-library(IRanges)
-
 read.bed <- function(filename, genome, stranded = F){
     if (!file.exists(filename)){
-        stop(paste("Error in read.bed(filename, genome, stranded): filename",
+        stop(paste("In read.bed(filename, genome, stranded): filename",
                    filename, " not found"))
     }
 
-    bed <- read.table(filename, sep = "\t", header = F, stringsAsFactors = F)
+    if (! genome in c('hg19','hg38','mm9','mm10')){
+        stop("in read.bed(filename, genome, stranded): Invalid Genome")
+    }
+
+    bed <- readr::read.table(filename, sep = "\t", header = F, stringsAsFactors = F)
 
     if (!grepl("chr", bed[,1])){
-        stop("Error in read.bed(filename, genome, stranded): file not in
+        stop("in read.bed(filename, genome, stranded): file not in
         correct format, missing chr numbers")
     }
 
-    if (typeof(head(bed[,2])) != "numeric" | typeof(head(bed[,2])) != "integer"){
-        stop("Error in read.bed(filename, genome, stranded): file not in
+    if (typeof(head(bed[,2])) != "integer"){
+        stop("in read.bed(filename, genome, stranded): file not in
              correct format, second column not numeric")
     }
 
-    if (typeof(head(bed[,3])) != "numeric" | typeof(head(bed[,3]))!= "integer"){
-        stop("Error in read.bed(filename, genome, stranded): file not in
+    if (typeof(head(bed[,3]))!= "integer"){
+        stop("in read.bed(filename, genome, stranded): file not in
              correct format, third column not numeric")
     }
 
     if (stranded == T & (bed[1,6] != "+" | bed[1,6] != "-")){
-        stop("Error in read.bed(filename, genome, stranded): strandedness
+        stop("in read.bed(filename, genome, stranded): strandedness
              column not in correct format")
     }
 
-    gR <- GenomicRanges::GRanges(seqnames = bed[,1],
+    size_code = sprintf('%s_chrom_sizes', genome)
+    data(list = size_code, package = "annotatr")
+    seqlengths = get(size_code)
+
+    if(stranded){
+        gR <- GenomicRanges::GRanges(seqnames = bed[,1],
                       ranges = IRanges::IRanges(
                           start = bed[,2],
-                            end = bed[,3])
-    )
-
-    if (stranded == TRUE){
-        gR.strand = bed[,6]
+                            end = bed[,3]),
+                            strand = bed[,6],
+                            type = bed$type,
+                            seqlengths = seqlengths
     }
 
-    genome
+    else {
+        gR <- GenomicRanges::GRanges(seqnames = bed[,1],
+                                     ranges = IRanges::IRanges(
+                                         start = bed[,2],
+                                         end = bed[,3]),
+                                        strand = '*',
+                                     type = bed$type,
+                                     seqlengths = seqlengths
+    }
+
+    gR <- genomicRanges::trim(gR)
 }
 
 
