9e893d12018d1d3f8222bf073f82ce7e189278b0
Raymond Cavalcante <rcavalca@umich.edu>
2015-12-01 07:26:30 -0500

    Clarified error messages in R/Read.R
    * Also fixed some indentation wonkiness.

--- R/Read.R
+++ R/Read.R
@@ -1,62 +1,62 @@
 #' @export
 read.bed <- function(filename, genome, stranded = F){
     if (!file.exists(filename)){
-        stop(paste("In read.bed(filename, genome, stranded): filename",
-                   filename, " not found"))
+        stop(paste("In read.bed(filename, genome, stranded): file:",
+            filename, "not found."))
     }
 
     if (! genome %in% c('hg19','hg38','mm9','mm10')){
-        stop("in read.bed(filename, genome, stranded): Invalid Genome")
+        stop("In read.bed(filename, genome, stranded): Invalid genome.
+            Valid genomes are: hg19, hg38, mm9, and mm10.")
     }
 
+    # Read the file
     bed <- read.table(filename, sep = "\t", header = F, stringsAsFactors = F)
 
     if (!all(grepl("chr", bed[,1]))){
-        stop("in read.bed(filename, genome, stranded): file not in
-        correct format, missing chr numbers")
+        stop("In read.bed(filename, genome, stranded): file not in
+            correct format, chromosomes expected to start with 'chr'.")
     }
 
     if (typeof(head(bed[,2])) != "integer"){
-        stop("in read.bed(filename, genome, stranded): file not in
-             correct format, second column not numeric")
+        stop("In read.bed(filename, genome, stranded): file not in
+             correct format, start positions must be integers.")
     }
 
     if (typeof(head(bed[,3]))!= "integer"){
-        stop("in read.bed(filename, genome, stranded): file not in
-             correct format, third column not numeric")
+        stop("In read.bed(filename, genome, stranded): file not in
+             correct format, end positions must be integers.")
     }
 
     if (stranded == T & (bed[1,6] != "+" | bed[1,6] != "-")){
-        stop("in read.bed(filename, genome, stranded): strandedness
-             column not in correct format")
+        stop("In read.bed(filename, genome, stranded): with stranded = T,
+          strand column should contain +/- only.")
     }
 
+    # Get chromosome sizes from data/
     size_code = sprintf('%s_chrom_sizes', genome)
     data(list = size_code, package = "annotatr")
     seqlengths = get(size_code)
 
     if(stranded){
-        gR <- GenomicRanges::GRanges(seqnames = bed[,1],
-                      ranges = IRanges::IRanges(
-                          start = bed[,2],
-                            end = bed[,3]),
-                            strand = bed[,6],
-                            type = bed$type,
-                            seqlengths = seqlengths)
+        gR <- GenomicRanges::GRanges(
+            seqnames = bed[,1],
+            ranges = IRanges::IRanges(
+            start = bed[,2],
+            end = bed[,3]),
+            strand = bed[,6],
+            type = bed$type,
+            seqlengths = seqlengths)
     } else {
         gR <- GenomicRanges::GRanges(seqnames = bed[,1],
-                                     ranges = IRanges::IRanges(
-                                         start = bed[,2],
-                                         end = bed[,3]),
-                                        strand = '*',
-                                     type = bed$type,
-                                     seqlengths = seqlengths)
+            ranges = IRanges::IRanges(
+            start = bed[,2],
+            end = bed[,3]),
+            strand = '*',
+            type = bed$type,
+            seqlengths = seqlengths)
     }
 
     gR <- GenomicRanges::trim(gR)
     gR
 }
-
-
-
-
