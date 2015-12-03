c43e5b729a1138b606cf720c52fe183759c2c3e9
hhabra <hhabra1@gmail.com>
2015-12-03 12:26:19 -0500

    Updated tests for read_bed

--- tests/testthat/test_2_read.R
+++ tests/testthat/test_2_read.R
@@ -2,7 +2,7 @@ context('Test read_bed() function')
 
 test_that('Test error for file does not exist.',{
     expect_error(
-        read_bed("Nonexistent File", genome = 'hg38', stranded = F),
+        read_bed("Nonexistent File", genome = 'hg38', stranded = FALSE),
         'not found'
     )
 })
@@ -12,7 +12,7 @@ test_that('Correct Lengths.' , {
 
     ###NOTE TO SELF: NOT SURE IF DATA HAS + or - STRAND COLUMN ###
 
-    gR1 <- read_bed(filename1, 'hg19', stranded = F)
+    gR1 <- read_bed(filename1, 'hg19', stranded = FALSE)
 
     expect_equal(length(gR1), expected = 9848)
 })
