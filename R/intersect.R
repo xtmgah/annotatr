6d98b0fe391df9b81e7dc5965feadbd447af188f
Raymond Cavalcante <rcavalca@umich.edu>
2015-12-03 09:37:35 -0500

    Parse annotation shortcuts in intersect_annotations()
    * cpgs, basic_genes, and detailed_genes are keyword shortcuts for
    groups of built-in annotations.
    * Added an example to the documentation using one of the shortcuts.

--- R/intersect.R
+++ R/intersect.R
@@ -6,15 +6,28 @@
 #' elements of the annotation data (subject).
 #'
 #' @param regions The GRanges object of the user-data returned by read.bed().
-#' @param annotations A character vector of annotations to overlap with user-data. Valid annotation codes can be found with supported_annotations(). The "basic_genes" shortcut annotates regions to the 1-5Kb, promoter, 5UTR, CDS, and 3UTR knownGene regions. The "detailed_genes" shortcut annotates regions to the 1-5Kb, promoter, 5UTR exon/intron, CDS exon/intron, and 3UTR exon/intron knownGene regions. The "cpgs" shortcut annotates regions to the CpG islands, shores, shelves, and interCGI regions.
+#' @param annotations A character vector of annotations to overlap with user-data. Valid annotation codes can be found with supported_annotations(). The "basic_genes" shortcut annotates regions to the 1-5Kb, promoter, 5UTR, exon, intron, and 3UTR knownGene regions. The "detailed_genes" shortcut annotates regions to the 1-5Kb, promoter, 5UTR exon/intron, CDS exon/intron, and 3UTR exon/intron knownGene regions. The "cpgs" shortcut annotates regions to the CpG islands, shores, shelves, and interCGI regions.
 #' @param genome One of the genomes in supported_genomes(). Should match argument used in read.bed().
 #' @param ignore.strand A boolean indicating whether strandedness should be respected in findOverlaps().
 #'
 #' @return A list of Hits objects giving the indices of objects overlapping in query and subject for each annotation.
 #'
 #' @examples
+#' A very simple example with only 3 genomic regions
 #' bed = system.file('extdata', 'test_intersect.bed', package = 'annotatr')
 #' annotations = c('hg19_cpg_islands','hg19_cpg_shores','hg19_knownGenes_promoters')
+#'
+#' d = read_bed(filename = bed, genome = 'hg19', stranded = FALSE)
+#'
+#' i = intersect_annotations(
+#'   regions = d,
+#'   annotations = annotations,
+#'   genome = 'hg19',
+#'   ignore.strand = TRUE)
+#'
+#' A more complicated example using Gm12878 Pol2 ChIP-seq from ENCODE and an annotation shortcut
+#' bed = system.file('extdata', 'Gm12878_Pol2.narrowPeak.gz', package = 'annotatr')
+#' annotations = c('basic_genes')
 #'
 #' d = read_bed(filename = bed, genome = 'hg19', stranded = FALSE)
 #'
@@ -41,6 +54,17 @@ intersect_annotations = function(regions, annotations, genome, ignore.strand) {
 
     stop(sprintf('Error in intersect_annotations(...): "%s" is(are) not supported. See supported_annotations().',
       paste(unsupported, collapse=', ')))
+  }
+
+  # Check for shortcut annotation accessors 'cpgs', 'basic_genes', or 'detailed_genes'
+  # and create the right annotations based on the genome
+  if(annotations == 'cpgs') {
+    annotations = paste(genome, 'cpg', c('islands','shores','shelves','inter'), sep='_')
+  } else if (annotations == 'basic_genes') {
+    annotations = paste(genome, 'knownGenes', c('1to5kb','promoters','5UTRs','exons','introns','3UTRs'), sep='_')
+  } else if (annotations == 'detailed_genes') {
+    annotations = paste(genome, 'knownGenes',
+      c('1to5kb','promoters','exons5UTRs','introns5UTRs','exonsCDSs','intronsCDSs','exons3UTRs','introns3UTRs'), sep='_')
   }
 
   # Collect the annotation objects into a GRangesList
