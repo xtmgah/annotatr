3f4cb9eb65288520cebaaa2e95fcf7fb10f789f9
Raymond Cavalcante <rcavalca@umich.edu>
2015-12-04 17:24:45 -0500

    Updates to read_bed()
    * read_bed() now uses the genome argument for the GenomicRanges
    metadata.
    * Check to see if input data is sorted, if not, sort it.
    * Also enforce uniqueness of the regions in input data. This is
    particularly a problem with some ENCODE data that we happen to be using
    for testing.
    * Added a test to ensure unique() is working.

--- tests/testthat/test_2_read.R
+++ tests/testthat/test_2_read.R
@@ -7,15 +7,16 @@ test_that('Test error for file does not exist.',{
     )
 })
 
-test_that('Correct Lengths.' , {
+test_that('Test correct length when no duplicates' , {
     filename1 = system.file('extdata', 'K562_Cjun.narrowPeak.gz', package = 'annotatr')
-
-    ###NOTE TO SELF: NOT SURE IF DATA HAS + or - STRAND COLUMN ###
-
     gR1 <- read_bed(filename1, 'hg19', stranded = FALSE)
 
     expect_equal(length(gR1), expected = 9848)
 })
 
+test_that('Test unique statement works', {
+    file = system.file('extdata', 'Gm12878_Pol2.narrowPeak.gz', package = 'annotatr')
+    gr = read_bed(file, 'hg19', stranded = FALSE)
 
-
+    expect_equal( 27558 - length(gr), expected = 5821)
+})
