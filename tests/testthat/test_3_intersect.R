79051ca3d5d9b699d936a7d623fe86cf04d9c556
Raymond Cavalcante <rcavalca@umich.edu>
2015-12-06 14:57:44 -0500

    Combine existing annotate.R with intersect.R
    * I want to eventually use annotate.R to contain the package wrapper
    annotate() function. This function will combine all the steps for the
    user.
    * Changed the name of tabulate_intersections() to
    annotate_intersections(), which I think is clearer.

--- tests/testthat/test_3_intersect.R
+++ tests/testthat/test_3_intersect.R
@@ -1,6 +1,6 @@
-context('Test intersect_annotations() function')
+context('Test intersection and annotation of intersection')
 
-test_that('Test error thrown for non-GRanges regions object.',{
+test_that('Test error thrown for non-GRanges regions object in intersect_annotations()',{
   bed = system.file('extdata', 'test_intersect.bed', package = 'annotatr')
   annotations = c('hg19_cpg_islands')
   d = read_bed(filename = bed, genome = 'hg19', stranded = F)
@@ -15,7 +15,7 @@ test_that('Test error thrown for non-GRanges regions object.',{
   )
 })
 
-test_that('Test error thrown for unsupported genome.',{
+test_that('Test error thrown for unsupported genome in intersect_annotations()',{
   bed = system.file('extdata', 'test_intersect.bed', package = 'annotatr')
   annotations = c('hg19_cpg_islands')
   d = read_bed(filename = bed, genome = 'hg19', stranded = F)
@@ -30,7 +30,7 @@ test_that('Test error thrown for unsupported genome.',{
   )
 })
 
-test_that('Test error thrown for unsupported annotation.',{
+test_that('Test error thrown for unsupported annotation in intersect_annotations()',{
   bed = system.file('extdata', 'test_intersect.bed', package = 'annotatr')
   annotations = c('hg19_cpg_islands','big_willy_style')
   d = read_bed(filename = bed, genome = 'hg19', stranded = F)
@@ -45,7 +45,7 @@ test_that('Test error thrown for unsupported annotation.',{
   )
 })
 
-test_that('Test error thrown for basic_genes and detailed_genes.',{
+test_that('Test error thrown for basic_genes and detailed_genes in intersect_annotations()',{
   bed = system.file('extdata', 'test_intersect.bed', package = 'annotatr')
   annotations = c('basic_genes', 'detailed_genes')
   d = read_bed(filename = bed, genome = 'hg19', stranded = F)
@@ -60,7 +60,7 @@ test_that('Test error thrown for basic_genes and detailed_genes.',{
   )
 })
 
-test_that('Test dual annotation shortcut.',{
+test_that('Test dual annotation shortcut in intersect_annotations()',{
   bed = system.file('extdata', 'Gm12878_Pol2.narrowPeak.gz', package = 'annotatr')
   annotations = c('basic_genes','cpgs')
 
@@ -76,7 +76,7 @@ test_that('Test dual annotation shortcut.',{
 })
 
 
-test_that('Test correct intersections.',{
+test_that('Test correct intersections in intersect_annotations()',{
   bed = system.file('extdata', 'test_intersect.bed', package = 'annotatr')
   annotations = c('hg19_cpg_islands','hg19_cpg_shores','hg19_knownGenes_promoters')
 
@@ -91,4 +91,28 @@ test_that('Test correct intersections.',{
   query_hits = sapply(i, function(a){a@queryHits})
 
   expect_equal( all(query_hits == c(3,2,1)) , expected = TRUE)
+})
+
+test_that('Test annotate_intersections() works',{
+  bed = system.file('extdata', 'Gm12878_Pol2.narrowPeak.gz', package = 'annotatr')
+  annotations = c('basic_genes','cpgs')
+
+  d = read_bed(filename = bed, genome = 'hg19', stranded = F, use.score = FALSE)
+
+  i = intersect_annotations(
+    regions = d,
+    annotations = annotations,
+    genome = 'hg19',
+    ignore.strand = T)
+
+  t = annotate_intersections(
+    regions = d,
+    intersections = i,
+    use.score = FALSE)
+
+  # Get number of annotations from i and t and make sure they're the same
+  i_lengths = sort(sapply(i, length))
+  t_lengths = sort(table(t$annot_type))
+
+  expect_equal( all(i_lengths == t_lengths) , expected = TRUE)
 })
