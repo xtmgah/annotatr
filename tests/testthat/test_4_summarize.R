8f55348f19d4ce03a418e5ff56684f7d977d4c8e
Raymond Cavalcante <rcavalca@umich.edu>
2015-12-08 13:04:16 -0500

    Summarize functions + altered test data
    * Added summary of score over annotations and summary of names over
    annotations. The output of these functions will feed directly into the
    visualization functions.
    * In an effort to reduce the size of inst/extdata, various files were
    reduced by subsampling, or removing unused columns. There should be
    sufficient variety of data types in inst/extdata for the vignette.

---
+++ tests/testthat/test_4_summarize.R
@@ -0,0 +1,51 @@
+context('Test score and name summaries over annotations')
+
+test_that('Test error on incorrect input class', {
+  bed = system.file('extdata', 'Gm12878_Ezh2_sorted_scores.narrowPeak.gz', package = 'annotatr')
+
+  expect_error(summarize_score(bed), 'must have class tbl_df')
+})
+
+test_that('Test ', {
+  bed = system.file('extdata', 'Gm12878_Ezh2_sorted_scores.narrowPeak.gz', package = 'annotatr')
+  annotations = c('basic_genes','cpgs')
+
+  d = read_bed(filename = bed, genome = 'hg19', stranded = F, use.score = TRUE)
+
+  i = intersect_annotations(
+    regions = d,
+    annotations = annotations,
+    genome = 'hg19',
+    ignore.strand = T)
+
+  t = annotate_intersections(
+    regions = d,
+    intersections = i,
+    use.score = TRUE)
+
+  s = summarize_score(t)
+
+  expect_equal( mean(s[['mean']]), expected = 25.19482, tolerance = 0.01)
+})
+
+test_that('', {
+  bed = system.file('extdata', 'IDH2mut_v_NBM_names_scores_chr9.txt.gz', package = 'annotatr')
+  annotations = c('basic_genes','cpgs')
+
+  d = read_bed(filename = bed, genome = 'hg19', stranded = F, use.score = TRUE)
+
+  i = intersect_annotations(
+    regions = d,
+    annotations = annotations,
+    genome = 'hg19',
+    ignore.strand = T)
+
+  t = annotate_intersections(
+    regions = d,
+    intersections = i,
+    use.score = TRUE)
+
+  s = summarize_name(t)
+
+  expect_equal( sum(s[['n']]), expected = 19984)
+})
