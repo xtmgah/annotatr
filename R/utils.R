556348671e9b6690af1dc833fb0a94144fcbc3f0
Raymond Cavalcante <rcavalca@umich.edu>
2015-12-12 18:03:29 -0500

    Added tidy_annotations() for visualization legends
    * Takes the annotation accessors and makes them more pleasant to look
    at for figure labels.

--- R/utils.R
+++ R/utils.R
@@ -27,3 +27,28 @@ supported_annotations = function() {
 supported_genomes = function() {
   return(c('hg19','hg38','mm9','mm10'))
 }
+
+#' Function to tidy up annotation accessors for visualization
+#'
+#' @param annotations A character vector of annotations, in the order they are to appear in the visualization.
+#'
+#' @return A character vector of tidied annotation names
+#'
+#' @examples
+#' tidy_annotations(annotations = c('hg19_cpg_islands','hg19_knownGenes_promoters'))
+tidy_annotations = function(annotations) {
+  tidy = sapply(annotations, function(a){
+    tokens = unlist(strsplit(a,'_'))
+    if(tokens[2] == 'cpg') {
+      if(tokens[3] == 'inter') {
+        return('interCGI')
+      } else {
+        return(paste('CpG', tokens[3]))
+      }
+    } else if (tokens[2] == 'knownGenes') {
+      return(tokens[3])
+    }
+  })
+
+  return(tidy)
+}
