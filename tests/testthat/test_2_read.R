d6f89bb4cb4b086d0d8deadbb9f597be6eb77566
Raymond Cavalcante <rcavalca@umich.edu>
2016-01-08 11:47:59 -0500

    Added read_annotations() to R/read.R
    * This function allows users to read in a file (see doc for details) to
    be used for custom annotations in the downstream functions.
    * Tested and documented.
    * TO DO: Add hooks to annotate_regions() and functions in R/utils.R
    * Removed extraneous extdata

--- tests/testthat/test_2_read.R
+++ tests/testthat/test_2_read.R
@@ -1,7 +1,7 @@
 context('Test read module')
 
 ################################################################################
-# Test errors
+# Test errors in read_bed()
 
   test_that('Test error for file does not exist.',{
     expect_error(
@@ -21,7 +21,7 @@ context('Test read module')
     file = system.file('extdata', 'test_read_error_chr.bed', package = 'annotatr')
 
     expect_error(read_bed(file, genome = 'hg19', stranded = TRUE, use.score = TRUE),
-      'First column of BED file does not appear to be chromsome')
+      'First column of BED file does not appear to be chromosome')
   })
 
   test_that('Test integer column errors' , {
@@ -118,4 +118,116 @@ context('Test read module')
       use.score = TRUE)
 
     expect_equal( dplyr::setequal(colnames(GenomicRanges::mcols(gr)), c('name','pval','diff_meth','mu1','mu0')), expected = TRUE)
+  })
+
+################################################################################
+# Test pre-read errors in read_annotations()
+
+  test_that('Test error for file does not exist',{
+    expect_error(
+      read_annotations(file = 'hello', genome = 'hg19', annotation_name = 'test'),
+      'not found'
+      )
+  })
+
+  test_that('Test error for NULL genome',{
+    file = system.file('extdata', 'test_annotations_3.bed', package='annotatr')
+
+    expect_error(
+      read_annotations(file = file, annotation_name = 'test'),
+      'argument "genome" is missing'
+      )
+  })
+
+################################################################################
+# Test column handling in read_annotations()
+
+  test_that('Test chrom error',{
+    file = system.file('extdata', 'test_annotations_3_error_chr.bed', package='annotatr')
+
+    expect_error(
+      read_annotations(file = file, genome = 'hg19', annotation_name = 'custom'),
+      'First column of annotation file does not appear'
+    )
+  })
+
+  test_that('Test integer column 1 error',{
+    file = system.file('extdata', 'test_annotations_3_error_int1.bed', package='annotatr')
+
+    expect_error(
+      read_annotations(file = file, genome = 'hg19', annotation_name = 'custom'),
+      'Second column of annotation file must'
+    )
+  })
+
+  test_that('Test integer column 2 error',{
+    file = system.file('extdata', 'test_annotations_3_error_int2.bed', package='annotatr')
+
+    expect_error(
+      read_annotations(file = file, genome = 'hg19', annotation_name = 'custom'),
+      'Third column of annotation file must'
+    )
+  })
+
+  test_that('Test duplicate name warning',{
+    file = system.file('extdata', 'test_annotations_4_warning_dup.bed', package='annotatr')
+
+    expect_warning(
+      read_annotations(file = file, genome = 'hg19', annotation_name = 'custom'),
+      'Some annotations have duplicated names'
+    )
+
+    test = read_annotations(file = file, genome = 'hg19', annotation_name = 'custom')
+    expect_equal(all(test$ID == c('custom:1', 'custom:2', 'custom:3')), expected = TRUE)
+  })
+
+  test_that('Test invalid strand ',{
+    file = system.file('extdata', 'test_annotations_5_error_strand.bed', package='annotatr')
+
+    expect_error(
+      read_annotations(file = file, genome = 'hg19', annotation_name = 'custom'),
+      'Strand column should contain only'
+    )
+  })
+
+  test_that('Test not 3, 4, or 5 columns', {
+    file = system.file('extdata', 'test_read_multiple_data_nohead.bed', package='annotatr')
+
+    expect_error(
+      read_annotations(file = file, genome = 'hg19', annotation_name = 'custom'),
+      'The file used to create custom annotations must'
+      )
+  })
+
+################################################################################
+# Test read_annotations() overall
+
+  test_that('Test 3 columns works', {
+    file = system.file('extdata', 'test_annotations_3.bed', package='annotatr')
+
+    hg19_custom_test = read_annotations(file = file, genome='hg19', annotation_name = 'test')
+
+    expect_equal(length(hg19_custom_test), expected = 3)
+    expect_equal(all(hg19_custom_test$ID == c('test:1','test:2','test:3')), expected = TRUE)
+    expect_equal(as.logical(GenomeInfoDb::genome(hg19_custom_test) == 'hg19'), expected = TRUE)
+  })
+
+  test_that('Test 4 columns works', {
+    file = system.file('extdata', 'test_annotations_4.bed', package='annotatr')
+
+    mm14_custom_test = read_annotations(file = file, genome='mm14', annotation_name = 'test')
+
+    expect_equal(length(mm14_custom_test), expected = 3)
+    expect_equal(all(mm14_custom_test$ID == c('test1','test2','test3')), expected = TRUE)
+    expect_equal(as.logical(GenomeInfoDb::genome(mm14_custom_test) == 'mm14'), expected = TRUE)
+  })
+
+  test_that('Test 5 columns works', {
+    file = system.file('extdata', 'test_annotations_5.bed', package='annotatr')
+
+    hg19_custom_TFBS = read_annotations(file = file, genome='hg19', annotation_name = 'TFBS')
+
+    expect_equal(length(hg19_custom_TFBS), expected = 3)
+    expect_equal(all(hg19_custom_TFBS$ID == c('test1','test3','test2')), expected = TRUE)
+    expect_equal(as.logical(GenomeInfoDb::genome(hg19_custom_TFBS) == 'hg19'), expected = TRUE)
   })
